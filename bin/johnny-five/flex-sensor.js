5ff3aa2ac7bfb719db44bd5078d3ec847f6d2eab
Tak Tran <contact@tutaktran.com>
2014-10-26 10:16:37 +0000

    Add flex sensor example.

---
+++ bin/johnny-five/flex-sensor.js
@@ -0,0 +1,33 @@
+/**
+ * Flex sensor example
+ *
+ * Similar setup to flexSensor (light dependent resistor)
+ *
+ * Based off Johnny-five example: https://github.com/rwaldron/johnny-five/blob/master/docs/flexSensor.md
+ */
+var five = require("johnny-five");
+
+var board = new five.Board();
+
+var pins = {
+  flex: "A1"
+};
+
+board.on("ready", function() {
+
+  var flexSensor = new five.Sensor({
+    // Connect one side to power, and one
+    // side to the the pin. Connect the pin also to a
+    // 47k resistor, through to ground.
+    pin: pins.flex,
+    freq: 250
+  });
+
+  board.repl.inject({
+    flex: flexSensor
+  });
+
+  flexSensor.on("data", function() {
+    console.log(this.value);
+  });
+});
