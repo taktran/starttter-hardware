7d4e8b950811d15be4c65f4f0da28f326e97cb2d
Tak Tran <contact@tutaktran.com>
2014-10-26 02:19:18 +0000

    Simplify led example.

--- bin/johnny-five/led.js
+++ bin/johnny-five/led.js
@@ -12,27 +12,14 @@ var board = new five.Board();
 // Set up of the board
 var pins = {
   led: 13,          // Negative end in GND
-  pullUpButton: 2   // Other pin in GND
 };
 
 board.on("ready", function() {
   var led = new five.Led(pins.led);
 
-  var button = new five.Button({
-    pin: pins.pullUpButton,
-    isPullup: true
-  });
-
   this.repl.inject({
-    led: led,
-    button: button
+    led: led
   });
 
-  button.on("down", function(value) {
-    led.on();
-  });
-
-  button.on("up", function() {
-    led.off();
-  });
+  led.strobe(500);
 });
