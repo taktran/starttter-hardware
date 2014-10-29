c499edefa7869268b57f1ada39a2cca60895c4f3
Tak Tran <contact@tutaktran.com>
2014-10-29 01:46:17 +0000

    Load colour from spark.

--- app/src/app.js
+++ app/src/app.js
@@ -1,5 +1,26 @@
 (function() {
   "use strict";
 
-  console.log("I'm alive!");
+  var stateUrl = "https://api.spark.io/v1/devices/" + CONFIG.sparkCoreId + "/state?access_token=" + CONFIG.accessToken;
+
+  function setCoreColour(colorObj) {
+    var rgbStr = colorObj.r + "," + colorObj.g + "," + colorObj.b;
+    $(".rgb-color").css("background-color", "rgb(" + rgbStr + ")");
+  }
+
+  $.get(stateUrl)
+    .done(function(data) {
+      var result = data["result"];
+
+      try {
+        var colorObj = JSON.parse(result);
+        setCoreColour(colorObj);
+      } catch(err) {
+        console.log("Parse error", result, err);
+      }
+    })
+    .fail(function(err) {
+      console.log("Get state error", err);
+    });
+
 })();
